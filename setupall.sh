9a268397ce53fd300196cffb1181b04a65dd3550
Wizard of z/OS <wizard@zdevops.com>
2023-02-20 20:52:08 +0100

    Smarter installer, less space requirements

--- setupall.sh
+++ setupall.sh
@@ -3,62 +3,57 @@
 # Setting up ZOSOpenTools the way it should be :)
 # wizardofzos 2023
 
-# Get bash, perl, ncurses and git
-echo "Getting Bash"
-curl -k -L -s https://github.com/ZOSOpenTools/bashport/releases/download/bashport_518/bash-5.2.20230210_170433.zos.pax.Z --output bash.pax.Z 
-echo "Getting Perl"
-curl -k -L -s https://github.com/ZOSOpenTools/perlport/releases/download/perlport_531/perl5-blead.20230210_213003.zos.pax.Z --output perl.pax.Z
-echo "Getting Ncurses"
-curl -k -L -s https://github.com/ZOSOpenTools/ncursesport/releases/download/ncursesport_521/ncurses-6.3.20230210_180151.zos.pax.Z --output ncurses.pax.Z
-echo "Getting Git"
-curl -k -L -s https://github.com/ZOSOpenTools/gitport/releases/download/gitport_519/git-2.39.1.20230210_171810.zos.pax.Z --output git.pax.Z 
-
-
-# get all the stuff in the right place.....delete targets first
-echo "Un/de paxing...."
 # clear previous muck
 rm -rf gitzot
 mkdir gitzot
 
 
-# Un(de?)pax bash: Get directory it will extract to, and rename/move to bash
-echo "Bash..."
+# Get bash, perl, ncurses and git
+echo "Getting bash"
+curl -k -L -s https://github.com/ZOSOpenTools/bashport/releases/download/bashport_518/bash-5.2.20230210_170433.zos.pax.Z --output bash.pax.Z 
+echo " - unpaxing.."
 pax -v -f bash.pax.Z > tmpfile
 bashpath=`head -n 1 tmpfile | cut -c55-100`
 pax -rf bash.pax.Z
 rm bash.pax.Z
+echo " - moving into place"
 mv $bashpath gitzot/bash
 rm tmpfile
 
-# Un(de?)pax perl: Get directory it will extract to, and rename/move to perl
-echo "Perl..."
+echo "Getting perl"
+curl -k -L -s https://github.com/ZOSOpenTools/perlport/releases/download/perlport_531/perl5-blead.20230210_213003.zos.pax.Z --output perl.pax.Z
+echo " - unpaxing.."
 pax -v -f perl.pax.Z > tmpfile
 perlpath=`head -n 1 tmpfile | cut -c55-100`
 pax -rf perl.pax.Z
 rm perl.pax.Z
+echo " - moving into place"
 mv $perlpath gitzot/perl
 rm tmpfile
 
-# Un(de?)pax ncurses: Get directory it will extract to, and rename/move to ncurses
-echo "Ncurses..."
+echo "Getting ncurses"
+curl -k -L -s https://github.com/ZOSOpenTools/ncursesport/releases/download/ncursesport_618/ncurses-6.3.20230219_035409.zos.pax.Z --output ncurses.pax.Z
+echo " - unpaxing.."
 pax -v -f ncurses.pax.Z > tmpfile
 ncursespath=`head -n 1 tmpfile | cut -c55-100`
 pax -rf ncurses.pax.Z
 rm ncurses.pax.Z
+echo " - moving into place"
 mv $ncursespath gitzot/ncurses
 rm tmpfile
 
-# Un(de?)pax git: Get directory it will extract to, and rename/move to git
-echo "Git..."
+echo "Getting git"
+curl -k -L -s https://github.com/ZOSOpenTools/gitport/releases/download/gitport_519/git-2.39.1.20230210_171810.zos.pax.Z --output git.pax.Z 
+echo " - unpaxing.."
 pax -v -f git.pax.Z > tmpfile
 gitpath=`head -n 1 tmpfile | cut -c55-100`
 pax -rf git.pax.Z
 rm git.pax.Z
+echo " - moving into place"
 mv $gitpath gitzot/git
 rm tmpfile
 
-
-
+echo "All done, packaging with zdopack"
 # Use code from to zdopack craft a proper receiver (that takes care of all the .env stuff :) 
 srcuss=`pwd`/gitzot
 workdir=".pack-work-dir"
